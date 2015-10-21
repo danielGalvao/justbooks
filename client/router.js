bbfe6dd75bd1ccd17b92f6673d255cba217db8e2
Daniel Galvão <galvaodev@gmail.com>
2015-10-21 00:32:00 -0200

    Removing files and update route

--- client/router.js
+++ client/router.js
@@ -1,8 +1,19 @@
 // Rotas são definidas neste arquivo
-
 Router.route('/', {
 	name: 'home',
 	action: function () {
 		this.render('home');
+	}
+});
+Router.route('/users', {
+	name: 'users',
+	action: function () {
+		this.render('users');
+	}
+});
+Router.route('/books', {
+	name: 'books',
+	action: function () {
+		this.render('books');
 	}
 });
