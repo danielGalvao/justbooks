907ef2b15ef0d261ed61b6435559b2c43905ce6f
Daniel Galvão <galvaodev@gmail.com>
2015-12-02 00:27:15 -0200

    Add getJSON to google books api

--- client/books/books.js
+++ client/books/books.js
@@ -1,30 +1,19 @@
 // 	 code only runs on the client
 Template.books.helpers({
-
 	//retornar os livros salvos no banco
 	books: function() {
 		return Books.find({}, {sort: {createdAt: -1}});
 	},
 });
+Template.newbook.events({
+	'keyup input[name="title"]' : function(e){
+	var _search = $('input[name="title"]').val();
+	if(_search.length && _search.length%3 === 0){
+		$.getJSON('https://www.googleapis.com/books/v1/volumes?q=title+'+_search,function(){
 
-Template.books.events({
-	// Submit de novo usuario
-	"submit .new-book": function (event) {
-		// Prevent default browser form submit
-		event.preventDefault();
-
-		// TODO: Pega valores dos elementos
-	
-
-		// Inserindo usuario
-		Books.insert({
-			nome: nome,
-			autor: autor,
-			paginas: paginas,
-			status: status,
-			capa: capa,
-			createdAt: new Date()
 		});
+	}
+  }
 
-	},
 });
+
