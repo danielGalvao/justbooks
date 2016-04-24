d5352ac59f026530ae76a720d03beb9317ff8463
Daniel Galvão <galvaodev@gmail.com>
2016-04-24 00:42:29 -0300

    Add remove book function and update css to show image

--- client/books/list/list.js
+++ client/books/list/list.js
@@ -5,10 +5,15 @@ Template.listBooks.helpers({
 	},
 });
 Template.eachBook.events({
-	'click .bookDesc' : function(event, template){
+	'click .bookDesc' : function(){
 		$(".card[data-bk="+this._id+"] .card-reavel").addClass('active');
 	},
-	'click .closeDesc' : function(event, template){
+	'click .closeDesc' : function(){
 		$(".card[data-bk="+this._id+"] .card-reavel").removeClass('active');
-	}
+	},
+	'click .removeBook' : function () {
+		if (confirm('Tem certeza que deseja remover esse livro?')) {
+				Books.remove(this._id);
+		}
+  }
 });
